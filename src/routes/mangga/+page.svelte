44ada29aaaf77f55f86eca0935c65de76c8df337
Rid <alridho2003@gmail.com>
2024-07-12 15:51:39 +0800

    added mangga page

---
+++ src/routes/mangga/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Mangga from '$lib/assets/mangga.jpg';
+</script>
+
+<div class="px-20 py-10 flex flex-col items-center">
+    <div>
+        <h1>Pohon Mangga</h1>
+        <img src={Mangga} alt="Mangga">
+    </div>
+    <div>
+        Mangga merupakan salah satu tanaman yang biasanya disebut sebagai mempelam atau nama ilmiahnya ialah Mangifera indica. Nama tersebut merupakan salah satu nama yang sekaligus dengan pohonnya. Mangga menjadi salah satu tanaman yang bermarga Mangifera terdiri atas 35 sampai 40 anggota serta suku Anacardiaceae. Tanaman manga ini berasal dari India dan juga Burma yang telah tersebar pada Asia Tenggara kira-kira 1500 tahun yang lalu. Pada umumnya budidaya tanaman mangga banyak ditanamn di daerah tropis, bahkan tak salah kalau mangga bertumbuh subur di negara Indonesia. Tanaman mangga tersebut memiliki jenis-jenis buah yang berbeda rasanya.
+    </div>
+</div>
