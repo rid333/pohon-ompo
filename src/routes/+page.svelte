8a02b7b8317bf6f649843c4b653f00157c3c2586
Rid <alridho2003@gmail.com>
2024-07-14 13:12:48 +0800

    updated mangga page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <div class="flex flex-col justify-center items-center">
-    <h1 class="text-xl">Koordinator Kabupaten</h1>
-    <h1 class="text-xl">Raehan</h1>
+    <a href="/">Pohon 1</a>
+    <a href="/">Pohon 2</a>
+    <a href="/">Pohon 3</a>
+    <a href="/">Pohon 4</a>
+    <a href="/">Pohon 5</a>
+    <a href="/">Pohon 6</a>
+    <a href="/">Pohon 7</a>
+    <a href="/">Pohon 8</a>
+    <a href="/">Pohon 9</a>
+    <a href="/">Pohon 10</a>
 </div>
--- src/routes/mangga/+page.svelte
+++ src/routes/mangga/+page.svelte
@@ -2,32 +2,25 @@
     import Mangga from '$lib/assets/mangga.jpg';
 </script>
 
-<div class="py-10">
-    <div>
-        <h1>Pohon Mangga</h1>
+<div class="flex flex-col gap-y-2">
+    <div class="flex flex-col text-xl font-bold">
         <img src={Mangga} alt="Mangga">
+        <p>Pohon Mangga</p>
+        <p>Mangifera indica</p>
     </div>
 
     <div>
-        Mangga merupakan salah satu tanaman yang biasanya disebut sebagai mempelam atau nama ilmiahnya ialah Mangifera indica. Nama tersebut merupakan salah satu nama yang sekaligus dengan pohonnya. Mangga menjadi salah satu tanaman yang bermarga Mangifera terdiri atas 35 sampai 40 anggota serta suku Anacardiaceae. Tanaman manga ini berasal dari India dan juga Burma yang telah tersebar pada Asia Tenggara kira-kira 1500 tahun yang lalu. Pada umumnya budidaya tanaman mangga banyak ditanamn di daerah tropis, bahkan tak salah kalau mangga bertumbuh subur di negara Indonesia. Tanaman mangga tersebut memiliki jenis-jenis buah yang berbeda rasanya.
+        Mangga merupakan salah satu tanaman yang biasanya disebut sebagai mempelam 
+        atau nama ilmiahnya ialah Mangifera indica. Nama tersebut merupakan salah satu 
+        nama yang sekaligus dengan pohonnya. Mangga menjadi salah satu tanaman yang bermarga 
+        Mangifera terdiri atas 35 sampai 40 anggota serta suku Anacardiaceae. Tanaman manga 
+        ini berasal dari India dan juga Burma yang telah tersebar pada Asia Tenggara kira-kira 
+        1500 tahun yang lalu. Pada umumnya budidaya tanaman mangga banyak ditanamn di daerah 
+        tropis, bahkan tak salah kalau mangga bertumbuh subur di negara Indonesia. Tanaman 
+        mangga tersebut memiliki jenis-jenis buah yang berbeda rasanya.
     </div>
 
     <div>
         Klasifikasi dari tanaman mangga
     </div>
-
-    <ol class="list-decimal">
-        <li>Kingdom : Plantae</li>
-        <li>Infra Kingdom : Streptophyta</li>
-        <li>Sub Kingdom : Viridiplantae</li>
-        <li>Divisi : Tracheophyta</li>
-        <li>Super Divisi : Embryophyta</li>
-        <li>Sub Divisi : Spermatophytina</li>
-        <li>Kelas : Magnoliopsida</li>
-        <li>Ordo : Sapindales</li>
-        <li>Super Ordo : Rosanae</li>
-        <li>Famili : Anacardiaceae</li>
-        <li>Genus : Mangifera L</li>
-        <li>Spesies : Mangifera Indica L</li>
-    </ol>
 </div>
