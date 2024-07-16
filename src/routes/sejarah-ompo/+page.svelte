ebe5888d585466b01fadc9c1b2cea34e6608ef93
Rid <alridho2003@gmail.com>
2024-07-16 10:40:39 +0800

    updated pages

---
+++ src/routes/sejarah-ompo/+page.svelte
@@ -0,0 +1,43 @@
+<script></script>
+
+<div class="flex flex-col gap-y-3 p-3">
+    <div>
+        Permandian Alam Ompo merupakan salah satu objek wisata yang terletak kurang lebih 3 km di sebelah Utara Kota Watan Soppeng, Kelurahan Ompo, Kecamatan Lalabata, Kabuguten Soppeng, Provinsi Sulawesi Selatan. Tempat wisata ini memiliki daya tarik kesegaran air yang langsung bersumber dari mata air alami di Kelurahan Ompo. Selain menjadi objek wisata, mata air Ompo jaga dimanfaatkan oleh masyarakat sekitar untuk kebutuhan sehari-hari seperti sumber perairan sawah hingga dimanfaatkan oleh seluruh masyarakat Kabupaten Soppeng.
+    </div>
+
+    <div>
+        Hal ini terjadi karena debit air Ompo yang cukup besar sepanjang tahun serta memiliki tingkat kejernihan yang sangat baik untuk diolah menjadi air kemasan. Dibalik ramainya pengunjung yang datang di Pemandian Ompo, terdapat kisah menarik mengenai asal muasal sumber mata air Ompo. Konon pada zaman baheula, menurut cerita rakyat Soppeng, menyatakan bahwa mata air Ompo adalah tempat munculnya seorang petani dengan kerbau dan alat pembajak sawahnya yang tenggelam di bagian Barat mata air yang disebut Labue. Labue berarti turun atau terdesak masuk ke dalam tanah.
+    </div>
+
+    <div>
+        Cerita berawal dari sumber mata air Ompo yang dulunya merupakan area persawahan penduduk. Ketika itu, penduduk setempat membajak sawah menggunakan sepasang kerbau. Suatu hari, ada seorang pangalung (petani) sedang membajak sawah miliknya yang begitu luas di sebelah Utara Madello bersama kerbaunya dari pagi hingga sore. Setelah itu, tiba saat sang petani dan kerbaunya bersiap untuk pulang. Namun, kaki kerbau milik sang petani menginjak sesuatu yang aneh. Secara bersamaan dari bekas injakan si kerbau keluar air yang sedikit demi sedikit menjadi semakin banyak, bahkan sampai di pusar sang petani. Sontak sang petani kaget melihat banyaknya air yang keluar dari tanah sehingga mencoba untuk naik ke punggung kerbau miliknya. Namun saat hendak naik, kaki si kerbau tersangkut tali pengikat hingga kerbau tidak bisa diam.
+    </div>
+
+    <div>
+        Setelah berusaha cukup lama akhirnya sang petani berhasil naik di punggung kerbaunya dengan niat ingin meninggalkan tempat itu. Namun, si kerbau tiba-tiba tidak mau bergerak seakan ada sosok makhluk tak kasat mata (makhluk halus) menghalangeya untuk bergerak. Petani itu hanya bisa berdoa agar keluar dari kubangan air tersebut. Hingga akhirnya sang petani berhasil keluar bersama dengan kerbaunya. Namun, kejadian aneh terjadi kembali, saat itu turun hujan deras disertal guntur. Tanah yang telah diinjak oleh kerbau bergeser ke tanah tempat kejadian aneh pertama kali terjadi. Tidak berselang lama, kerbau dan petani kini kembali terjebak hingga pusaran air mellit tubuh sang petani. Seperti yang terjadi sebelumnya, petani dan kerbaunya tidak bisa bergerak. Namun kali ini pusaran air itu perlahan menenggelamkan petani serta kerbaunya, tassobo (tenggelam ke dalam lumpur sawah) dan hilang.
+    </div>
+    <div>
+        Karena sang petani tak kunjung pulang, istri dan kedua anaknya resah. Hal itu membuat sang istri pergi untuk mencari suaminya hingga tengah malam, tetapi tidak juga menemukan suaminya. Pulanglah sang istri ke rumahnya dan melaksanakan shalat tahajud. Setelah melaksanakan shalat tahajud, tiba-tiba sang istri terbayang sosok suaminya yang mengucapkan "Saya sudah tidak ada Dik. Saya tenggelam di tempat munculnya air. Sayalah penunggu tempat itu. Ingatlah untuk selalu membawa kedua anak kita ke jalan yang benar".
+    </div>
+    <div>
+        Keesokan harinya, tersebarlah berita kematian sang perani. Hal tersebut sontak mengejutkan masyarakat kampung sekitar yang kemudian berbondong-bondong segera mencari sang petani hingga berhari-hari, namun tidak juga ditemukan. Hari demi haripun mulai berlalu. Pada suatu waktu, di sekitar tempat petani dan kerbaunya hilang terdapat sebuah Galung Mallobo (sawah yang sangat berlumpur). Tak di sangka, sang petani dan kerbaunya pun Mompo (muncul) di tempat tersebut. Saat bersamaan, air yang sangat jernih dan melimpah juga menyembur dari tempat di temukannya sang petani bersama kerbaunya.
+    </div>
+    <div>
+        Para masyarakat kampung pun berbondong-bondong untuk menyaksikan peristiwa ajaib tersebut. Usai peristiwa itu, masyarakat memberi nama mata air ini dengan "Mompo" atau "Ompo' yang artinya muncul dan bersepakat menjadikan mata air Ompo sebagai pemandian yang sekarang kita kenal dengan Pemandian Alam Ompa, Begitulah Ompo diberikan nama karena tempat munculnya air. Hari tersebut merupakan hari yang sangat bersejarah.
+    </div>
+    <div>
+        Hingga saat ini mata air Ompo tidak pernah kering, bahkan debit air yang cukup besar memeberikan sejuta manfaat untuk masyarakat scitar. Salah satu manfaat yang cukup besar untuk masyarakat adalah dengan mengembangkan daerah pertanian berkat pengaliran air yang sangat baik. Selain itu, berkat daya tarik secara historis dan fisiologis membuat pemerintah setempat mengembangkan daerah Ompo ini sebagai sektor wisata yang potensial. Bahkan Pemandian Alam Ompo ini sudah jadi ikon wisata di Kabupaten Soppeng yang banyak dikunjungi wisatawan lokal hingga mancanegara jauh sebelum Permandian Air Panas Lejja dan Pemandian Alam Citta dikenal masyarakat.
+    </div>
+
+    <div>
+        Permandian Alam Ompo juga memiliki tiga kolam, yaitu kolain renang khusus anak-anak, dewasa dan kolam sumber mata air. Untuk kolam sumber mata air yang berada di dalam lokasi obyek wisata tidak dapat digunakan untuk berendam sehingga diberikan pagar beton sebagai pembatasnya. Alasan pengunjung tidak boleh berendam di kolam sumber mata air karena masyarakat percaya ada nilai keramat dan mistis pada kolam tersebut. Kepercayaan tersebut manjadikan Permandian Alam Ompo menyimpan banyak sisi menarik, yaitu:
+    </div>
+
+    <div>
+        <ul class="list-decimal">
+            <li>Sebagian masyarakat Soppeng nanganggap kolam sumber mata air tersebut memiliki nilai mistis sehingga masyarakat pada hari-hari tertentu atau setiap telah melaksanakan hajat keluarga seperti pernikahan atau aqiqah datang ke Pemandian Ompo untuk mandi dan mengirim sesaji berupa telur ayam kampung, pisang, dan sokko pitu rupa (nasi ketan tujuh warna) untuk dipersembahkan ke kolam sumber mata air Ompo.</li>
+            <li>Dahulu di mana dalam keadaan yang mendesak masyarakat datang ke sumber mata air Ompo berdoa kepada Tuhan agar sumber air Ompo bertambah debitnya dan tidak disangka saat masyarakat belum meninggalkan tempat tersebut debit airnya sudah bertambah.</li>
+            <li>Permandian Alam Ompo juga dipercaya dapat mendatangkan jodoh bagi pengunjung yang lajang dengan mengambil air Ompo di sumber utamanya sebagai obat untuk mendapatkan jodoh secepatnya. Menurut masyarakat sekitar, pengunjung yang masih lajang dan telah mandi di tempat ini akan lebih mudah mendapatkan jodohnya. Namun bukan hanya itu saja, orang-orang yang datang ke pemandian Ompo biasanya tidak hanya sekedar mandi, tetapi juga datang untuk melakukan syukuran dengan memotong hasil ternak seperti ayam, sapi, dan kambing ketika hasil panennya melimpah</li>
+        </ul>
+    </div>
+</div>
